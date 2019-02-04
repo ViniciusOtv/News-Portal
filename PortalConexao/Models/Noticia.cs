ae54c2eb256bb6c030b46b09475e6c5a4ea3f410
vinicius.otv <vinicius.otv@hotmail.com>
2019-02-03 22:58:24 -0200

    My First Commit

---
+++ PortalConexao/Models/Noticia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalConexao.Models
+{
+    public class Noticia
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string SubTitulo { get; set; }
+
+        public string Imagem { get; set; }
+        
+    }
+}
